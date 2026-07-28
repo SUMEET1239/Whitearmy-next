19556ff244b2089b837eb9a4c69609a8c525648d
Sumeet Gupta <sumitg1239@gmail.com>
2026-07-28 20:00:39 +0530

    mongodb atlas issue resolve

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,10 +1,42 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI!;
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("MongoDB URI missing");
+}
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+let cached: MongooseCache = (global as any).mongoose || {
+  conn: null,
+  promise: null,
+};
+
+if (!(global as any).mongoose) {
+  (global as any).mongoose = cached;
+}
 
 export async function connectDB() {
-  if (mongoose.connection.readyState >= 1) return;
+  try {
+    if (cached.conn) {
+      return cached.conn;
+    }
 
-  await mongoose.connect(MONGO_URI);
-  console.log("MongoDB Connected");
+    if (!cached.promise) {
+      cached.promise = mongoose.connect(MONGODB_URI);
+    }
+
+    cached.conn = await cached.promise;
+
+    console.log("✅ MongoDB Connected");
+
+    return cached.conn;
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error);
+    throw error;
+  }
 }
